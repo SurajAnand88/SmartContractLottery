09dd68c27791939a6004d6891fff226641644385
SurajAnand88 <surajbloodstone@gmail.com>
2025-04-07 19:00:14 +0530

    Added anvil deploy script

--- Makefile
+++ Makefile
@@ -13,5 +13,5 @@ install:
 	forge install cyfrin/foundry-devops@0.2.2 --no-commit && forge install smartcontractkit/chainlink-brownie-contracts@1.1.1 && forge install foundry-rs/forge-std@v1.8.2 --no-commit && forge install transmissions11/solmate@v6 --no-commit
 
 deploy-sepolia:
-	@forge script script/DeployRaffle.s.sol:DeployRaffle --rpc-url https://eth-sepolia.g.alchemy.com/v2/SpNLu8QnfuaXgpmqHVAgauoMF0uY_L96 --account default --broadcast
+	@forge script script/DeployRaffle.s.sol:DeployRaffle --rpc-url https://eth-sepolia.g.alchemy.com/v2/SpNLu8QnfuaXgpmqHVAgauoMF0uY_L96 --account SepoliaWallet broadcast
 
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,6 +13,9 @@ abstract contract GetChainIds {
     uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
     int256 public constant MOCK_WEI_PER_UNIT_LINK = 4e15;
 
+    address public FOUNDRY_DEFAULT_SENDER =
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
     uint256 public constant ETH_SEPOLIA_CHAINID = 11155111;
     uint256 public constant LOCAL_CHAINID = 31337;
 }
@@ -62,7 +65,7 @@ contract HelperConfig is Script, GetChainIds {
             subscriptionId: 90408319092427918692949180483022735124383284755486992522396419863317981140589,
             callBackGasLimit: 500000,
             link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
-            account: 0x6ABdf655b0d4BED6a7305EC7DE3902883c0045a8
+            account: 0xC3Ad970d7d6f8b0807835b92d7c5D65CFc792295
         });
     }
 
